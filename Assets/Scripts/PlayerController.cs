dd8576d06dd51ba2ef3a03c1357c63e1fd689aac
acozy03 <acozy03@gmail.com>
2024-10-08 14:01:11 -0400

    implemented running and flip direction animations in script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,35 @@ public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f; 
     Vector2 moveInput; 
+    [SerializeField]
+    private bool _isMoving = false; 
+    public bool IsMoving { get {
 
-    public bool IsMoving { get; private set; }
+        return _isMoving; 
+    }
     
+    private set {
+
+        _isMoving = value; 
+        animator.SetBool("isMoving", value); 
+    }
+    }
+    
+    public bool _isFacingRight = true; 
+    public bool IsFacingRight { get { return _isFacingRight; }
+    private set{
+        if(_isFacingRight != value){
+            transform.localScale *= new Vector2(-1, 1); 
+        }
+        _isFacingRight = value; 
+    } }
     Rigidbody2D rb; 
 
+    Animator animator; 
     private void Awake() 
     {
         rb = GetComponent<Rigidbody2D>(); 
+        animator = GetComponent<Animator>(); 
     }
     // Start is called before the first frame update
     void Start()
@@ -38,5 +59,19 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>(); 
 
         IsMoving = moveInput != Vector2.zero; 
+
+        SetFacingDirection(moveInput);
     }
+
+    private void SetFacingDirection(Vector2 moveInput) {
+
+        if(moveInput.x > 0 && !IsFacingRight) {
+            IsFacingRight = true; 
+        }
+
+        else if(moveInput.x < 0 && IsFacingRight) {
+            IsFacingRight = false; 
+        }
+    }
+
 }
