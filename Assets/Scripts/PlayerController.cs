7764e4ea3a615c345fd86ced6a616a32e9a58eb7
acozy03 <acozy03@gmail.com>
2024-10-08 19:00:03 -0400

    no longer stuck on wall after jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,26 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingSpaceDirections))]
 public class PlayerController : MonoBehaviour
 {
-    Animator animator; 
-    public float walkSpeed = 5f; 
-    Vector2 moveInput; 
+
+    Rigidbody2D rb;
+    Animator animator;
+    Vector2 moveInput;
+    TouchingSpaceDirections touchingDirections;
+
+    public float walkSpeed = 5f;
+    private float jumpImpulse = 10f;
+
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+            if (IsMoving && !touchingDirections.IsOnWall) return walkSpeed; return 0;
+        }
+    } 
+
+
     [SerializeField]
     private bool _isMoving = true;
-    TouchingSpaceDirections touchingDirections;
-    private float jumpImpulse = 10f;
-    Rigidbody2D rb;
     public bool IsMoving { get {
 
         return _isMoving; 
@@ -44,7 +56,7 @@ public class PlayerController : MonoBehaviour
    
     private void FixedUpdate() 
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
         animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
     }
     public void OnMove(InputAction.CallbackContext context) 
