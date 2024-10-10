764409b37abdb51eec2df25edf6794337543030c
acozy03 <acozy03@gmail.com>
2024-10-10 10:18:55 -0400

    updates and double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingSpaceDirections))]
 public class PlayerController : MonoBehaviour
@@ -13,13 +13,18 @@ public class PlayerController : MonoBehaviour
     TouchingSpaceDirections touchingDirections;
 
     [SerializeField]
-    public float airWalkSpeed = 5f; 
+    public float airWalkSpeed = 5f;
 
     [SerializeField]
     public float walkSpeed = 5f;
 
     [SerializeField]
     private float jumpImpulse = 10f;
+
+    [SerializeField]
+    private int maxJumpCount = 2;  // Maximum number of jumps (for double jump)
+
+    private int jumpCount = 0;  // Tracks how many jumps have been performed
 
     public float CurrentMoveSpeed
     {
@@ -31,88 +36,100 @@ public class PlayerController : MonoBehaviour
                 {
                     return walkSpeed;
                 }
-
                 else
                 {
                     return airWalkSpeed;
                 }
             }
-            
             return 0;
         }
-    } 
-
+    }
 
     [SerializeField]
     private bool _isMoving = true;
-    public bool IsMoving { get {
-
-        return _isMoving; 
-    }
-    
-    private set {
-
-        _isMoving = value; 
-        animator.SetBool(AnimationStrings.isMoving, value); 
-    }
-    }
-    
-    public bool _isFacingRight = true; 
-    public bool IsFacingRight { get { return _isFacingRight; }
-    private set{
-        if(_isFacingRight != value){
-            transform.localScale *= new Vector2(-1, 1); 
-        }
-        _isFacingRight = value; 
-    } }
-
-    private void Awake() 
+    public bool IsMoving
     {
-        rb = GetComponent<Rigidbody2D>(); 
+        get
+        {
+            return _isMoving;
+        }
+
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool(AnimationStrings.isMoving, value);
+        }
+    }
+
+    public bool _isFacingRight = true;
+    public bool IsFacingRight
+    {
+        get { return _isFacingRight; }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            _isFacingRight = value;
+        }
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         touchingDirections = GetComponent<TouchingSpaceDirections>();
     }
-   
-    private void FixedUpdate() 
+
+    private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
         animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
-    }
-    public void OnMove(InputAction.CallbackContext context) 
-    {
-        moveInput = context.ReadValue<Vector2>(); 
 
-        IsMoving = moveInput != Vector2.zero; 
+        // Reset jump count when grounded
+        if (touchingDirections.IsGrounded)
+        {
+            jumpCount = 0;  // Reset jump count when player is grounded
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
 
         SetFacingDirection(moveInput);
     }
 
-    private void SetFacingDirection(Vector2 moveInput) {
-
-        if(moveInput.x > 0 && !IsFacingRight) {
-            IsFacingRight = true; 
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
         }
-
-        else if(moveInput.x < 0 && IsFacingRight) {
-            IsFacingRight = false; 
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
         }
     }
 
-    public void OnJump(InputAction.CallbackContext context) {
-
-        if (context.started && touchingDirections.IsGrounded)
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started && (touchingDirections.IsGrounded || jumpCount < maxJumpCount))
         {
             animator.SetTrigger(AnimationStrings.jumpTrigger);
             rb.velocity = new Vector2(rb.velocity.x, jumpImpulse);
+            jumpCount++;  // Increment jump count after each jump
         }
     }
 
     public void OnAttack(InputAction.CallbackContext context)
     {
-        if(context.started)
+        if (context.started)
         {
             animator.SetTrigger(AnimationStrings.attackTrigger);
         }
     }
-
 }
