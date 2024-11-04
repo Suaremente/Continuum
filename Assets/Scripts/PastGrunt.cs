353d21f3e7a549fe100ee8634c73e175ac83d289
acozy03 <acozy03@gmail.com>
2024-11-03 21:03:03 -0500

    fixed issue for enemy wall glitch when building

--- Assets/Scripts/PastGrunt.cs
+++ Assets/Scripts/PastGrunt.cs
@@ -1,34 +1,30 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingSpaceDirections), typeof(Damageable))]
 public class PastGrunt : MonoBehaviour
 {
-
     public float walkSpeed = 3f;
-    public float walkStopRate = .02f;
+    public float walkStopRate = 0.02f;
     public DetectionZone attackZone;
-    public DetectionZone cliffDetectionZone; 
+    public DetectionZone cliffDetectionZone;
 
-
-    Rigidbody2D rb;
-    TouchingSpaceDirections touchingDirections;
+    private Rigidbody2D rb;
+    private TouchingSpaceDirections touchingDirections;
     private Animator animator;
-    Damageable damageable; 
+    private Damageable damageable;
 
-    public enum WalkableDirection { Left, Right } 
+    public enum WalkableDirection { Left, Right }
 
     private WalkableDirection _walkDirection;
     private Vector2 walkDirectionVector = Vector2.right;
 
-    public WalkableDirection WalkDirection { 
-    
-        get { return _walkDirection; }  
-        set {
-
+    public WalkableDirection WalkDirection
+    {
+        get { return _walkDirection; }
+        set
+        {
             if (_walkDirection != value)
             {
                 gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
@@ -36,25 +32,26 @@ public class PastGrunt : MonoBehaviour
                 if (value == WalkableDirection.Right)
                 {
                     walkDirectionVector = Vector2.left;
-
                 }
-                else if (value == WalkableDirection.Left) {
-
-                    walkDirectionVector = Vector2.right; 
-                } 
-                
+                else if (value == WalkableDirection.Left)
+                {
+                    walkDirectionVector = Vector2.right;
+                }
             }
             _walkDirection = value;
         }
     }
 
-    public bool _hasTarget = false; 
-    public bool HasTarget { get { return _hasTarget; } private set
+    public bool _hasTarget = false;
+    public bool HasTarget
+    {
+        get { return _hasTarget; }
+        private set
         {
             _hasTarget = value;
             animator.SetBool(AnimationStrings.hasTarget, value);
         }
-        }
+    }
 
     public bool CanMove
     {
@@ -62,8 +59,8 @@ public class PastGrunt : MonoBehaviour
         {
             return animator.GetBool(AnimationStrings.canMove);
         }
-
     }
+
     public float AttackCooldown
     {
         get
@@ -76,26 +73,38 @@ public class PastGrunt : MonoBehaviour
         }
     }
 
-    private void Awake() {
+    private bool isFlipping = false; // To manage flip state
+    private float flipCooldown = 0.1f; // Cooldown for flipping
+    private float lastFlipTime = -Mathf.Infinity; // Timestamp of last flip
 
-        rb = GetComponent<Rigidbody2D>(); 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
         touchingDirections = GetComponent<TouchingSpaceDirections>();
         animator = GetComponent<Animator>();
         damageable = GetComponent<Damageable>();
     }
 
-    private void FixedUpdate() {
-
-        if (touchingDirections.IsOnWall && touchingDirections.IsGrounded) {
-            FlipDirection(); 
+    private void FixedUpdate()
+    {
+        if (touchingDirections.IsOnWall && !isFlipping && Time.time > lastFlipTime + flipCooldown)
+        {
+            FlipDirection();
+            lastFlipTime = Time.time; // Update the last flip time
         }
+
+        // Check for cliff detection
+        if (cliffDetectionZone.detectedColliders.Count == 0 && touchingDirections.IsGrounded)
+        {
+            FlipDirection(); // Flip if no ground detected
+        }
+
         if (CanMove)
             rb.velocity = new Vector2(walkSpeed * walkDirectionVector.x, rb.velocity.y);
-
-        else rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, walkStopRate), rb.velocity.y);
+        else
+            rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, walkStopRate), rb.velocity.y);
     }
 
-    // Update is called once per frame
     void Update()
     {
         HasTarget = attackZone.detectedColliders.Count > 0;
@@ -108,24 +117,35 @@ public class PastGrunt : MonoBehaviour
 
     private void FlipDirection()
     {
+        isFlipping = true; // Set flipping state to true
+
         if (WalkDirection == WalkableDirection.Right)
         {
             WalkDirection = WalkableDirection.Left;
-
         }
         else if (WalkDirection == WalkableDirection.Left)
         {
             WalkDirection = WalkableDirection.Right;
         }
-     
+
+        // Perform the flip after a short delay
+        StartCoroutine(FlipDelay());
     }
+
+    private IEnumerator FlipDelay()
+    {
+        yield return new WaitForSeconds(flipCooldown); // Wait for the cooldown
+        isFlipping = false; // Allow flipping again
+    }
+
     public void OnHit(int damage, Vector2 knockback)
     {
         rb.velocity = new Vector2(knockback.x, rb.velocity.y + knockback.y);
-
     }
 
-    public void OnCliffDectected() { 
-        if(touchingDirections.IsGrounded) FlipDirection();
+    public void OnCliffDetected()
+    {
+        if (touchingDirections.IsGrounded)
+            FlipDirection();
     }
 }
