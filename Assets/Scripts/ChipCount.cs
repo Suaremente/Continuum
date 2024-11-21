7c9c2595ebf34412af95bc6aa376d9286a3f93d1
acozy03 <acozy03@gmail.com>
2024-11-21 14:54:52 -0500

    updates to chipcount

--- Assets/Scripts/ChipCount.cs
+++ Assets/Scripts/ChipCount.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ChipCount : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -16,7 +16,23 @@ public class ChipCount : MonoBehaviour
         {
             ChipNum++;
             Debug.Log("Chip collided");
-            ChipCount2.instance.increaseCoins(ChipNum); 
-        }
+            ChipCount2.instance.increaseCoins(ChipNum);
+
+            if (ChipNum == 50) {
+
+                SceneManager.LoadScene("boss1"); 
+            
+            }
+
+            if (ChipNum == 100) {
+
+               //SceneManager.LoadScene("boss2");
+            }
+
+            if (ChipNum == 150) {
+
+                //SceneManager.LoadScene("boss3");
+            }
+        } 
     }
 }
